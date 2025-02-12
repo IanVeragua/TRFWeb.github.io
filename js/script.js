f2d3f4a93c8713bd027b5c6dbb5ba93c1715c751
Hollowed Studio <ian.ibacache31@gmail.com>
2025-02-12 15:22:00 -0300

    commit

--- js/script.js
+++ js/script.js
@@ -1,15 +1,8 @@
 let currentSlide = 0;
 const slides = document.querySelectorAll('.slides');
 
-showSlide(currentSlide);
-
 function changeSlide(n) {
-    currentSlide += n;
-    if (currentSlide >= slides.length) {
-        currentSlide = 0;
-    } else if (currentSlide < 0) {
-        currentSlide = slides.length - 1;
-    }
+    currentSlide = (currentSlide + n + slides.length) % slides.length;
     showSlide(currentSlide);
 }
 
@@ -19,18 +12,5 @@ function showSlide(index) {
     });
 }
 
-setInterval(() => {
-    changeSlide(1);
-}, 3000);
-
-document.addEventListener("DOMContentLoaded", function () {
-    document.querySelectorAll(".card-body").forEach(card => {
-        let textContainer = card.querySelector(".text-container");
-        let toggleLabel = card.querySelector(".label-toggle");
-
-        // Verifica si el contenido supera el alto inicial
-        if (textContainer.scrollHeight > textContainer.clientHeight) {
-            toggleLabel.style.display = "block"; // Muestra "Leer más"
-        }
-    });
-});
+setInterval(() => changeSlide(1), 3000);
+showSlide(currentSlide);
